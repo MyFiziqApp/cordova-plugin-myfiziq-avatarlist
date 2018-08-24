2674b6906db6116eef5d089f38e705b615e9ae52
MyFiziq <amita@myfiziq.com>
2018-08-24 16:37:24 +0800

    First commit

---
+++ src/ios/AvatarList.m
@@ -0,0 +1,169 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include "TargetConditionals.h"
+
+#import <Cordova/CDV.h>
+#import "AvatarList.h"
+
+@interface AvatarList()
+
+@property(nonatomic, strong) NSString *accountId;
+@property(nonatomic, strong) NSString *appId;
+@property(nonatomic, strong) NSString *awsRegion;
+@property(nonatomic, strong) NSString *environment;
+@property(nonatomic, strong) AWSLambda *lambda;
+@property(nonatomic, strong) NSString *vendor;
+@property(nonatomic, strong) NSString *userToken;
+@end
+
+
+@implementation AvatarList
+
+
+-(AWSLambdaInvoker *)lambda {
+    if (!_lambda) {
+        _lambda = [AWSLambdaInvoker LambdaInvokerForKey: self.appId];
+        return _lambda;
+    }
+
+}
+
+
+-(void)requestAvatars:(CDVInvokedUrlCommand *)command {
+   __block BOOL responsed = NO;
+
+   NSDictionary *info = (NSDictionary *)[command.arguments objectAtIndex:0];
+   self.awsRegion = [info valueForKey: @"region"];
+   self.accountId = [info valueForKey: @"account_id"];
+   self.vendor = [info valueForKey: @"vendor_name"];
+   self.environment = [info valueForKey:@"env"];
+   self.appId = [info valueForKey: @"appId"];
+   self.userToken = [info valueForKey: @"token"];
+
+   NSString *lambdaName = [NSString stringWithFormat:@"arn:aws:lambda:%@:%@:function:%@-%@-avatargetall", self.awsRegion, self.accountId, self.vendor, self.environment];
+   NSDictionary *headers = @{@"MFZ-Token":self.userToken, @"MFZ-AppID":self.appId};
+
+   [[self.lambda invokeFunction:lambdaName JSONObject:@{@"headers":headers}] continueWithBlock:^id _Nullable(AWSTask * _Nonnull t) {
+
+       dispatch_async(dispatch_get_main_queue(), ^{
+               responsed = YES;
+
+               NSLog(@"avatar response %@", t.result);
+
+               if (t.error) {
+                   NSLog(@"Error requesting avatars %@", t.error);
+
+               } else if (!t.result) {
+                   // Empty response
+                   //completionBlock(@[], nil);
+
+               } else if (![(NSNumber *)t.result[@"statusCode"] isEqualToNumber:@200] || [(NSString *)t.result[@"body"] isEqualToString:@""]) {
+                   // Invalid response
+                   //completionBlock(nil, [MFZErrors errorWithCode:kMFZErrorCodeAvatarSyncServerInvalidResp desc:_MFZC_ERR_SERVER_ERR reason:_MFZ_ERR_SDK_REG_UNKNOWN recovery:_MFZC_ERR_CONTACT_DEV]);
+               } else {
+                   //completionBlock([self parseResponse:t.result[@"body"]], nil);
+               }
+
+       });
+
+       return nil;
+
+   } cancellationToken:cancellationTokenSource.token];
+
+   dispatch_time_t time =  dispatch_time(DISPATCH_TIME_NOW, (int64_t)(45.0 * NSEC_PER_SEC));
+   dispatch_after(time, dispatch_get_main_queue(), ^{
+
+       if(!responsed) {
+           if (completionBlock) {
+               [cancellationTokenSource cancel];
+               completionBlock(nil, [MFZErrors errorWithCode:kMFZErrorCodeNoConnection desc:_MFZC_ERR_CONNECTION reason:nil recovery:_MFZC_ERR_NET_CHECK]);
+           }
+       }
+   });
+
+
+}
+
+- (NSString*)uniqueAppInstanceIdentifier:(UIDevice*)device
+{
+    NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
+    static NSString* UUID_KEY = @"CDVUUID";
+    
+    // Check user defaults first to maintain backwards compaitibility with previous versions
+    // which didn't user identifierForVendor
+    NSString* app_uuid = [userDefaults stringForKey:UUID_KEY];
+    if (app_uuid == nil) {
+        if ([device respondsToSelector:@selector(identifierForVendor)]) {
+            app_uuid = [[device identifierForVendor] UUIDString];
+        } else {
+            CFUUIDRef uuid = CFUUIDCreate(NULL);
+            app_uuid = (__bridge_transfer NSString *)CFUUIDCreateString(NULL, uuid);
+            CFRelease(uuid);
+        }
+
+        [userDefaults setObject:app_uuid forKey:UUID_KEY];
+        [userDefaults synchronize];
+    }
+    
+    return app_uuid;
+}
+
+- (void)getDeviceInfo:(CDVInvokedUrlCommand*)command
+{
+    NSDictionary* deviceProperties = [self deviceProperties];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:deviceProperties];
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (NSDictionary*)deviceProperties
+{
+    UIDevice* device = [UIDevice currentDevice];
+
+    return @{
+             @"manufacturer": @"Apple",
+             @"model": [device modelVersion],
+             @"platform": @"iOS",
+             @"version": [device systemVersion],
+             @"uuid": [self uniqueAppInstanceIdentifier:device],
+             @"cordova": [[self class] cordovaVersion],
+             @"isVirtual": @([self isVirtual])
+             };
+}
+
++ (NSString*)cordovaVersion
+{
+    return CDV_VERSION;
+}
+
+- (BOOL)isVirtual
+{
+    #if TARGET_OS_SIMULATOR
+        return true;
+    #elif TARGET_IPHONE_SIMULATOR
+        return true;
+    #else
+        return false;
+    #endif
+}
+
+@end
